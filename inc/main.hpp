1f8db97886af4c586204e8bc06ccab0df3147246
snakehater <vigor.turujlija@gmail.com>
2021-11-09 13:53:37 +0100

    Shaders up and running

--- inc/main.hpp
+++ inc/main.hpp
@@ -2,9 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
-//#include <GL/glew.h>
-//#include <GL/glut.h>
+#include <unistd.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -13,14 +11,9 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 
-//#include <GL/glx.h>
-
 // math functions
 #include <glm/glm.hpp>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
-
-// #include <GL/gl.h>
-// #include <GL/glu.h>
