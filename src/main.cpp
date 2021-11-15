4124f30512de8dc1381263905a04201708ffe7d0
snakehater <vigor.turujlija@gmail.com>
2021-11-15 13:38:53 +0100

    Locked mouse while in our glfw view

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,7 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
 	
 	// configure global opengl state
 	// -----------------------------
